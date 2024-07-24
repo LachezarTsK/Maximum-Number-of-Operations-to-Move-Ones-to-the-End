2d7c6dc773e8fb272f2fb9bd66313a0ece7b118f
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-07-24 20:33:48 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,16 @@
+
+/**
+ * @param {string} binaryString
+ * @return {number}
+ */
+var maxOperations = function (binaryString) {
+    const ASCII_ZERO = 48;
+    let countOnes = binaryString.codePointAt(0) - ASCII_ZERO;
+    let maxOperations = 0;
+
+    for (let i = 1; i < binaryString.length; ++i) {
+        countOnes += binaryString.codePointAt(i) - ASCII_ZERO;
+        maxOperations += binaryString[i - 1] > binaryString[i] ? countOnes : 0;
+    }
+    return maxOperations;
+};
